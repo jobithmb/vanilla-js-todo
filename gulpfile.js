44b2407abab0ee2e0c4203b0db122b1dba03e217
Jobith M Basheer <jobith@three38inc.com>
2025-05-05 20:55:57 +0530

    updates to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
+const gulp = require('gulp');
 const { parallel } = require('gulp');
 const { series } = require('gulp');
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
-function html() {
+function serve(cb) {
+  browserSync.init({
+      server: {
+          baseDir: "./dist"
+      }
+  });
+
+  gulp.watch("./src/scss/**/*.scss", css);
+  gulp.watch("./src/scripts/**/*.js", javascript);
+  gulp.watch("./src/images/**/*", images);
+  gulp.watch("./src/*.html", html);
+
+  cb();
+}
+
+function html(cb) {
   return src('./src/*.html')
     .pipe(dest('./dist'));
 }
 
-function javascript() {
+function javascript(cb) {
   return src('./src/scripts/**/*.js')
     .pipe(dest('./dist/scripts'));
 }
@@ -19,9 +36,10 @@ function css(cb) {
     .pipe(dest('./dist/css'));
 }
 
-function images() {
+function images(cb) {
   return src('./src/images/**/*',{encoding: false})
     .pipe(dest('./dist/images'));
 }
 
 exports.build = parallel(html, javascript, css, images);
+exports.default = series(html, javascript, css, images, serve);
